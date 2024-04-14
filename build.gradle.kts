0f92805442ad8121d602cee48f1b724150f83dfb
NyanCatForEver <cricotu30@gmail.com>
2024-04-14 11:39:56 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "com.google.android.material.color.utilities"
+version = "9cf7bd3"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    compileOnly("com.google.errorprone:error_prone_annotations:2.15.0")
+    compileOnly("androidx.annotation:annotation:1.2.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    //withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.jar
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "material-color-utilities"
+
